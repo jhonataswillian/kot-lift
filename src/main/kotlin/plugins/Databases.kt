38fa25b7ee049734b1648c4f3db9004888dfd699
Jhonatas Willian <jhonatas.willian.dev@gmail.com>
2026-02-23 18:09:37 -0300

    feat: externalize database configuration to application.conf

--- src/main/kotlin/plugins/Databases.kt
+++ src/main/kotlin/plugins/Databases.kt
@@ -3,15 +3,21 @@ package com.example.plugins
 import com.example.database.Exercises
 import com.example.database.Workouts
 import io.ktor.server.application.*
-import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 
 fun Application.configureDatabases() {
+    val driverClassName = environment.config.property("storage.driverClassName").getString()
+    val jdbcURL = environment.config.property("storage.jdbcURL").getString()
+    val user = environment.config.property("storage.user").getString()
+    val password = environment.config.property("storage.password").getString()
+
     val database = Database.connect(
-        url = "jdbc:h2:file:./build/db",
-        user = "root",
-        driver = "org.h2.Driver",
-        password = "",
+        url = jdbcURL,
+        user = user,
+        driver = driverClassName,
+        password = password,
     )
 
     transaction {
